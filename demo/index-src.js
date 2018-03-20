dd34ec66b7f6d36e6b24cd39be59fb365e1fd166
Kuma Li <li.jun.kuma@gmail.com>
2018-03-20 18:06:37 +0800

    Test: add init test

--- demo/index-src.js
+++ demo/index-src.js
@@ -1,39 +1,29 @@
-'use strict';
-
-var _electron = require('electron');
-
-var _electron2 = _interopRequireDefault(_electron);
-
-var _configBuilt = require('./config-built');
-
-var _configBuilt2 = _interopRequireDefault(_configBuilt);
-
-var _lib = require('../lib');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+import electron from 'electron';
+import oauth2Config from './config-built';
+import { oauth2Delegate } from '../lib';
 // Module to control application life.
-var app = _electron2.default.app;
+const app = electron.app;
 // Module to create native browser window.
-var BrowserWindow = _electron2.default.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow;
 
-var path = require('path');
-var url = require('url');
+
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = void 0;
+let mainWindow;
 
-function createWindow() {
+function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1024, height: 768 });
+  mainWindow = new BrowserWindow({width: 1024, height: 768})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -43,32 +33,32 @@ function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-(0, _lib.oauth2Delegate)(_configBuilt2.default);
+oauth2Delegate(oauth2Config);
